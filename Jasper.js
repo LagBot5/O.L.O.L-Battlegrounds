b5fc2bb04f48bab05a0a26c93ca02a6b09d52f3e
JasperDBu <167567956+JasperDBu@users.noreply.github.com>
2024-09-20 10:11:20 -0400

    Update Jasper.js

--- Jasper.js
+++ Jasper.js
@@ -1,3 +1,11 @@
+class element{
+	constructor (name, strength, weakness){
+		this.Element = name
+		this.strength = strength
+		this.weakness = weakness
+	}
+}
+
 class Character {
 	constructor ( name, fightingStyle, element, health, mana, img, skill1, skill2, skill3, skill4){
 		this.name = name
@@ -10,3 +18,16 @@ class Character {
 		this. selectedSkills = []
 	}
 }
+
+let elements = [
+	new element ( 'fire', 'air', 'water' ),
+	new element ( 'water', 'fire', 'earth' ),
+	new element ('earth', 'water', 'air'),
+	new element ( 'air', 'earth', 'fire')
+]
+
+
+let Characters = [
+	new Character( 'Dante', 'Warror', elements[0], 100, 80, 'not here yet', 'fireball', 'Homemade Flamethrower', 'Gasoline', 'BigFire' ),
+	new Character( 'Mr. Lam', 'Mage', elements[4], 100, 80, 'not here yet', 'Coding', 'Smash', 'Defense', 'racket throw')
+]
