c8b88202a2a6b9405ecfe042e5ad68516c603a30
JasperDBu <167567956+JasperDBu@users.noreply.github.com>
2024-10-03 22:23:25 -0400

    Update Jasper.js

--- Jasper.js
+++ Jasper.js
@@ -1,39 +1,52 @@
 // Timer and Turn based fighting code
 
-let TimeLimit = 10
-	let countdown
+let TimeLimit = 0
+let countdown
+let SelectedFighters
+let turn1 = 1
+let turn2 = 0
+let CurrentPlayer
+	let TargetedPlayer
+	let SelectedAction
+	
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(100);
+	SelectedFighters = [ Characters[0], Characters [1]]
+	print (SelectedFighters)
+}
 
 function draw() {
-	SelectedFighters = [ Characters[0], Characters [1]]
 	Fighting()
 }
 
 function Fighting() {
+	
 	let CurrentTime = int(millis()/1000)
 	countdown = TimeLimit - CurrentTime
 	if (countdown <= 0){
 		TurnEnd(SelectedFighters)
-		print('turnEnd')
-		TimeLimit += 10
+		TimeLimit += 5
 	}
+	
 	print(CurrentTime)
 }
 
-function TurnEnd(Players) {
-	let turn1 = 0
-	let turn2 = 1
+function TurnEnd( Players ) {
 	let Action
-	let CurrentPlayer
-	let TargetedPlayer
-	
-
-		CurrentPlayer = Players[turn1]
-		TargetedPlayer = Players[turn2]
-		Action = CurrentPlayer.SkillLibrary[0]
-		CurrentPlayer.action(Action)
-		turn1 = ( turn1 + 1 ) % Players.length 
-		turn2 = ( turn2 + 1 ) % Players.length
-		print ('turn end')
+		
+	Action = SelectedAction
+	if (Action){
+		CurrentPlayer.action(Action, TargetedPlayer)
+	}
+	turn1 = (turn1 + 1) % Players.length
+	turn2 = (turn2 + 1) % Players.length
+	CurrentPlayer = Players[turn1]
+	TargetedPlayer = Players[turn2]
+	print( "CurrentPlayer : " + CurrentPlayer.name)
+	print( "TargetedPlayer : " + TargetedPlayer.name)
+	SelectedAction = CurrentPlayer.SkillLibrary[0]
+	print ( turn1, turn2)
 }
 
 
@@ -94,18 +107,41 @@ class Character {
 		this.mana = mana
 		this.img =img
 		this.SkillLibrary = [ skill1, skill2, skill3, skill4]
+		this.status = 
+		this.StatusDur = 
 		this. selectedSkills = []
 	}
 	
-	action(Action){
+	action(Action, TargetedPlayer){
 		if (Action.type == 'attack'){
-			print('attack')
+			let dmg
+			let p2def
+			this.mana -= Action.manacost
+			dmg = Action.dmg
+			
+			if ( this.status && this.statusDur > 0 ) {
+				dmg += this.status.dmg
+				this.statusDur--
+			}	
+			if (TargetedPlayer )
+			
+			
+			if (TargetedPlayer.element == Action.element.weakness ){
+				dmg = dmg*1.10
+			}
+			TargetedPlayer.health -= (dmg*((100 - TargetedPlayer.defense)/100))
+			print( 'Player2 Health: ' + TargetedPlayer.health , 'mana: ' + this.mana)
 		}
+		
 		if (Action.type == 'buff'){
-			print('buff')
+			this.mana -= Action.manacost
+			this.status = Action
+			this.statusDur = Action.duration
 		}
+		
 		if (Action.type == 'debuff'){
-			print('debuff')
+			this.mana -= Action.manacost
+			TargetedPlayer.status = Action
 		}
 	}
 }
@@ -123,3 +159,4 @@ let Characters = [
 	new Character( 'Dante', 'Warror', elements[1], 100, 10, 80, 'not here yet', skills[0], skills[1], skills[2], skills[3] ),
 	new Character( 'Mr. Lam', 'Mage', elements[4], 100, 10, 80, 'not here yet', skills[4], skills[5], skills[6], skills[7])
 ]
+
