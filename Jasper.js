bea066499aa5bea57c944e04cd0fb44bb718704e
JasperDBu <167567956+JasperDBu@users.noreply.github.com>
2024-10-07 02:14:09 -0400

    Update Jasper.js

--- Jasper.js
+++ Jasper.js
@@ -1,6 +1,6 @@
 // Timer and Turn based fighting code
 
-let TimeLimit = 0
+let TimeLimit = 2
 let countdown
 let SelectedFighters
 let turn1 = 1
@@ -9,15 +9,20 @@ let CurrentPlayer
 	let TargetedPlayer
 	let SelectedAction
 	
-function setup() {
+
+	function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(100);
-	SelectedFighters = [ Characters[0], Characters [1]]
 	print (SelectedFighters)
+	
+	
 }
 
 function draw() {
+	clear()
 	Fighting()
+	SelectedFighters = [ Characters[0], Characters [1]]
+	DrawBars()
 }
 
 function Fighting() {
@@ -26,18 +31,34 @@ function Fighting() {
 	countdown = TimeLimit - CurrentTime
 	if (countdown <= 0){
 		TurnEnd(SelectedFighters)
-		TimeLimit += 5
+		TimeLimit += 2
 	}
 	
 	print(CurrentTime)
 }
 
+
+function DrawBars(){
+	
+	P1HealthBar.draw()
+	P1HealthBar.update(SelectedFighters[0].health)
+	
+	P1ManaBar.draw()
+	P1ManaBar.update(SelectedFighters[0].mana)
+	
+	P2HealthBar.draw()
+	P2HealthBar.update(SelectedFighters[1].health)
+	
+	P2ManaBar.draw()
+	P2ManaBar.update(SelectedFighters[1].mana)
+}
+
 function TurnEnd( Players ) {
 	let Action
-		
 	Action = SelectedAction
 	if (Action){
 		CurrentPlayer.action(Action, TargetedPlayer)
+		print( 'Previous Turn: ' + Action.name)
 	}
 	turn1 = (turn1 + 1) % Players.length
 	turn2 = (turn2 + 1) % Players.length
@@ -46,9 +67,7 @@ function TurnEnd( Players ) {
 	print( "CurrentPlayer : " + CurrentPlayer.name)
 	print( "TargetedPlayer : " + TargetedPlayer.name)
 	SelectedAction = CurrentPlayer.SkillLibrary[0]
-	print ( turn1, turn2)
 }
-
 
 //Class for elements 
 class element{
@@ -87,7 +106,11 @@ class buff extends skill{
 	}
 }
 
-
+let neutral
+let fire
+let water
+let earth
+let air
 
 
 //Calling Objects
@@ -108,21 +131,19 @@ let skills = [
 	new buff('rest', 0, 0, -30, neutral,'buff', 0, 1 ),
 	
 	//Dante
-	new skill('FireGetsuga', 20, 15, fire, 'attack'),
-	new skill('Amaterasu', 30, 25, fire, 'attack'),
+	new skill('FireGetsuga', 20, 40, fire, 'attack'),
+	new skill('Amaterasu', 30, 50, fire, 'attack'),
 	new buff('SuperSaiyan', 10, -10, 20, neutral, 'buff', 2),
 	new skill('FinalKamehameha', 75, 80, neutral, 'attack'),
 	// add charge up time for finalKamehameha
 	
 	// Mr Lam
 	new buff('Coffee', 7, 4,  15, neutral, 'buff', 3),
-	new skill('Smash', 15, 10, air, 'attack'),
+	new skill('Smash', 15, 25, air, 'attack'),
 	new buff('BadmintonDefense', 0, 20, 20, neutral, 'buff', 3),
 	new skill('Scooter',20, 15, air, 'attack')
 ]
 
-
-// Character
 class Character {
 	constructor ( name, fightingStyle, element, health, defense, mana, img, skill1, skill2, skill3, skill4){
 		this.name = name
@@ -170,13 +191,16 @@ class Character {
 			print( 'Player2 Health: ' + TargetedPlayer.health , 'mana: ' + this.mana)
 		}
 		
+		
 		if (Action.type == 'buff'){
 			if (this.statusDur <= 0 || !this.status){
+				this.mana -= Action.manacost
 				this.status = Action
 				this.statusDur = Action.duration
 			}
 			else {
 				this.statusDur--
+				TargetedPlayer.statusDur--
 				print("buff refused", "buffDur:" + this.statusDur)
 			}
 		}
@@ -192,5 +216,64 @@ class Character {
 let Characters = [
 	new Character( 'Dante', 'Warror', fire, 100, 10, 80, 'not here yet', skills[3], skills[4], skills[5], skills[6] ),
 	new Character( 'Mr. Lam', 'Mage', air, 100, 10, 80, 'not here yet', skills[7], skills[8], skills[9], skills[10])
+]
+
+//HealthBar and ManaBar
+class bar {
+	constructor(colour, w, h, x, y) {
+		this.colour = colour
+		this.w = w
+		this.barlength = w
+		this.h = h
+		this.x = x
+		this.y = y
+	}
+
+	draw() {
+		fill(this.colour)
+		rect(this.x, this.y, this.barlength, this.h)
+	}
+}
+
+class HealthBar extends bar {
+	constructor(colour, w, h, x, y) {
+		super(colour, w, h, x, y)
+	}
+
+	draw() {
+		super.draw()
+	}
+
+	update(HealthValue) {
+		if (HealthValue <= 0) {
+			HealthValue = 0
+		}
+		this.barlength = this.w * (HealthValue / 100)
+	}
+}
+
+class ManaBar extends bar {
+	constructor(colour, w, h, x, y) {
+		super(colour, w, h, x, y)
+	}
+
+	draw() {
+		super.draw()
+	}
+
+	update(ManaValue) {
+		if (ManaValue <= 0) {
+			ManaValue = 0
+		}
+		this.barlength = this.w * (ManaValue / 80)
+	}
+}
+
+let Bars = [
+	P1HealthBar = new HealthBar('green', 300, 50, 50, 50),
+	P2HealthBar = new HealthBar('green', -300, 50, 1000, 50),
+	P1ManaBar = new ManaBar('blue', 250, 30, 50, 120),
+	P2ManaBar = new ManaBar('blue', -250, 30, 1000, 120)
+
 ]
 
