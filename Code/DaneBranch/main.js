3da907e83833472ca6573b04bf89238658f89103
LagBot5 <167463120+LagBot5@users.noreply.github.com>
2025-02-03 14:25:54 -0500

    Update main.js
    Organizing

--- Code/DaneBranch/main.js
+++ Code/DaneBranch/main.js
@@ -1,1 +1,55 @@
+// mySketch
+const STARTING_SCREEN = 0
+const SELECTION_SCREEN = 1
+const CONFIRM_SCREEN = 2
+const PLAYING_SCREEN = 3
+let current_screen = STARTING_SCREEN
 
+function preload() {
+	FireArena = loadImage('game-battle-arena-background-with-hell-landscape-with-stone-circle-platform-hanging-metal-chains_154797-424.avif')
+	DanteIMG = loadImage('Dante.HEIC')
+	LamIMG = loadImage('Lam.HEIC')
+}
+
+function setup() {
+	createCanvas(1200, 800);
+	background("white");
+	FireArena.resize(1200, 600)
+}
+
+function draw() {
+	clear()
+	switch (current_screen) {
+		case STARTING_SCREEN:
+			StartingGraphic()
+			break;
+
+		case SELECTION_SCREEN:
+			SelectionScreenGraphic()
+			break;
+
+		case CONFIRM_SCREEN:
+			AbilityGraphic()
+			break;
+		case PLAYING_SCREEN:
+			PlayingScreen()
+
+			break;
+
+	}
+}
+
+function mouseClicked() {
+	switch (current_screen) {
+		case STARTING_SCREEN:
+			StartingButtons()
+			break;
+
+		case SELECTION_SCREEN:
+			ConfirmCharacter()
+			break;
+
+		case CONFIRM_SCREEN:
+			AbilityButtons()
+	}
+}
